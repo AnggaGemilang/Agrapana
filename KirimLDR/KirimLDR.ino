bf0fdfb63e623218149f970584fd474890ec9b08
AnggaGemilang <anggagemilang12@gmail.com>
2022-05-07 20:07:14 +0700

    update

--- KirimLDR/KirimLDR.ino
+++ KirimLDR/KirimLDR.ino
@@ -1,27 +1,105 @@
-#include <SoftwareSerial.h>
-SoftwareSerial ArduinoUno (5, 6); //RX,TX
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#define MSG_BUFFER_SIZE (50)
+#define LEDPin D1
+#define LightSensorPin A0
+#define WIFI_SSID "SPEEDY"
+#define WIFI_PASSWORD "suherman"
+#define MQTT_SERVER "test.mosquitto.org"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  if ((char)payload[0] == '0') {
+    digitalWrite(LEDPin, LOW);
+  } 
+  if ((char)payload[0] == '1') {
+    digitalWrite(LEDPin, HIGH);
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.publish("plantsgrowthchamber/lightsensor", "started light sensor");
+      client.publish("plantsgrowthchamber/led", "started LED");
+      client.subscribe("plantsgrowthchamber/led");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  //Open Serial Communication (Arduino-PC)
-  Serial.begin(57600);
+  Serial.begin(9600);
 
-  //Open Serial Communication (Arduino-NodeMCU)
-  ArduinoUno.begin(9600);
+  pinMode(LEDPin, OUTPUT);
+  pinMode(LightSensorPin, INPUT);
+  
+  setup_wifi();
+
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  //baca nilai ldr
-  int ldr1 = analogRead(0);
-  int ldr2 = analogRead(1);
-  int ldr3 = analogRead(2);
-  int ldr4 = analogRead(3);
-  String data = String(ldr1) + "#" + String(ldr2)+ "#" + String(ldr3)+ "#" + String(ldr4);
-  
-  //tampilkan ke serial monitor
-  Serial.println(data);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-  //kirim ke ESP8266  
-  ArduinoUno.print(data);
-
-  delay(5000);
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    int lightData = analogRead(LightSensorPin);
+    
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "value #%ld", lightData);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("plantsgrowthchamber/lightsensor", msg);
+  }
 }
