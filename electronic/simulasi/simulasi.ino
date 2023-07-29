6c8113e270748df44cb4cd2560560e978fcbecc5
AnggaGemilang <anggagemilang12@gmail.com>
2023-07-29 20:55:17 +0700

    update arduino done

--- electronic/simulasi/simulasi.ino
+++ electronic/simulasi/simulasi.ino
@@ -51,9 +51,18 @@ FuzzySet *hujanRingan = new FuzzySet(2.5, 11.25, 11.25, 20);
 FuzzySet *hujanSedang = new FuzzySet(15, 32.5, 32.5, 50);
 FuzzySet *hujanLebat = new FuzzySet(45, 72.5, 72.5, 100);  
 
-char output[200];
+char output[200], output2[150];
 unsigned int windTemperature, windHumidity, windPressure, windSpeed, lightIntensity, rainfall;
 unsigned int soilTemperature, soilHumidity, soilPh, soilNitrogen, soilPhosphor, soilKalium;
+char weatherForecast[50], pestsPrediction[100];
+
+double u0[] = {0.40427292089515005, -0.6348080638911874, -0.5826808357272043};
+double u1[] = {-0.48512750507418756, 0.761769676669425, 0.6992170028726451};
+
+double p0[] = {3.5232428618986744, 1.8252391089351945, 60.277945706722875};
+double p1[] = {0.7003134205554864, 2.0890725392537743, 0.7789572738382433};
+
+double c = 6.194008413911195;
 
 void setup() {
   Serial.begin(9600);
@@ -68,13 +77,9 @@ void loop() {
     reconnect();
   }
   client.loop();
-
   unsigned long now = millis();
-
   if (now - lastMsg > 4000) {
-    
     lastMsg = now;
-
     Serial.println("");
 
 //    Kirim data perangkat utama
@@ -132,7 +137,7 @@ void loop() {
     strcat(topic, "/pendukung/1");
     Serial.print("Topic: ");
     Serial.println(topic);
-    
+
     client.publish(topic, output);
     Serial.print("Publish message: ");
     Serial.println(output);
@@ -143,10 +148,75 @@ void loop() {
   }
 
   if (now - lastMsg2 > 10000) {
-    
     lastMsg2 = now;
+    Serial.println("");
+
+//    Fuzzy Logic
+
+    fuzzy->setInput(1, windTemperature);
+    fuzzy->setInput(2, windSpeed);
+    fuzzy->setInput(3, windHumidity);
+    fuzzy->fuzzify();
+    float output = fuzzy->defuzzify(1);
+
+    if(output <= 5)
+    {
+      strcpy(weatherForecast,"Cerah-Berawan");
+    } else if(output > 5 && output <= 20)
+    {
+      strcpy(weatherForecast,"Hujan Ringan");
+    } else if(output > 20 && output <= 50)
+    {
+      strcpy(weatherForecast,"Hujan Sedang");
+    } else
+    {
+      strcpy(weatherForecast,"Hujan Lebat");
+    } 
+
+//    Naive Bayes 
+
+    float data[3]= {soilTemperature, soilHumidity, rainfall};
+
+    double term1 = 0.0;
+    double term2 = 0.0;
+
+    for(int i=0; i<3; i++)
+    {
+        term1 += p0[i] * (data[i] - u0[i]) * (data[i] - u0[i]);
+        term2 += p1[i] * (data[i] - u1[i]) * (data[i] - u1[i]);
+    }
+
+    double temp = term1 - term2;
+
+    if(temp >= c)
+    {
+      strcpy(pestsPrediction,"ulat_daun=tinggi,ulat_krop=tinggi,busuk_hitam=tinggi"); 
+    }
+    else
+    {
+      strcpy(pestsPrediction,"ulat_daun=rendah,ulat_krop=rendah,busuk_hitam=rendah");
+    }
+
+//    Kirim data AI
+
+    StaticJsonDocument<200> doc3;
+
+    doc3["ai_processing"]["weather_forecast"] = weatherForecast;
+    doc3["ai_processing"]["pests_prediction"] = pestsPrediction;
+    serializeJson(doc3, output2);
+
+    strcpy(topic, "arnesys/");
+    strcat(topic, FIELD_CODE);
+    strcat(topic, "/utama/ai");
+    Serial.print("Topic: ");
+    Serial.println(topic);
+    
+    client.publish(topic, output2);
+    Serial.print("Publish message: ");
+    Serial.println(output2);
 
     Serial.println("");
+    Serial.println("=====================================");
 
 //    Kirim data perangkat utama
 
